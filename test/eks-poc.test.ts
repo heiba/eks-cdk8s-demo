fdaf3ccb91ef8e3f0cc8ebc956b374baad0ba26b
Mohamed Heiba <mohamed.adel.heiba@gmail.com>
2022-07-23 00:06:10 +0400

    Initial commit

---
+++ test/eks-poc.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as EksPoc from '../lib/eks-poc-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/eks-poc-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new EksPoc.EksPocStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
